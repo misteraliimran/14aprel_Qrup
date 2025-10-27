6afee8a154acfc86619c2910f0651adee7a63a29
misteraliimran <aaliimran027@gmail.com>
2025-10-27 20:57:02 +0400

    mfm

---
+++ codes/javase/src/lesson2/Main.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        String ad = "Əliimran";
+        String soyad = "Əliyev";
+        int yas = 12;
+
+        System.out.println("Ad: " + ad);
+        System.out.println("Soyad: " + soyad);
+        System.out.println("Yaş: " + yas);
+    }
+}
---
+++ codes/javase/src/lesson2/VariablesPractice.java
@@ -0,0 +1,21 @@
+package lesson2;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+
+        int age = 20;
+        System.out.println("age : " + age);
+
+        boolean hasHome = true;
+        System.out.println("hasHome : " + hasHome);
+
+        char studentGrade = 'A';
+        System.out.println("studentGrade : " + studentGrade);
+        
+        double productPrice = 3.8;
+        System.out.println("productPrice : " + productPrice);
+
+        String studentName = "Yaqub";
+        System.out.println("studentName : " + studentName);
+    }
+}
